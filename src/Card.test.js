ff393cfaa1b186d15e2cb2e02de459a212426d68
Brannen Petit <bpetit940@gmail.com>
2020-03-03 10:40:22 -0600

    created new branch and added Card.test.js contents

---
+++ src/Card.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Card from './Card';
+
+describe('<Card />', () => {
+    it('renders Card without crashing', () => {
+        const div = document.createElement('div');
+
+        ReactDOM.render(<Card title='String' content='another string'/>, div);
+
+        ReactDOM.unmountComponentAtNode(div);    
+    }) 
+
+    it('renders this UI as expected', () => {
+        const card = renderer.create(<Card title='string' content='anotherstring'/>).toJSON();
+        expect(card).toMatchSnapshot();
+    })
+    });
+
+  
